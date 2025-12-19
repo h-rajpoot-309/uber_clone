e6d0c6db3ed30aca8ef61fb3723d4c5591d1ded9
Haseeb Rajpoot <haseebrajpoots786@gmail.com>
2025-12-19 11:07:29 +0500

    bug fixed

--- lib/rider/controller/provider/tripProvider/rideRequestProvider.dart
+++ lib/rider/controller/provider/tripProvider/rideRequestProvider.dart
@@ -1,9 +1,11 @@
 import 'dart:developer';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uber_clone/common/model/pickupNDropLocationModel.dart';
+import 'package:flutter/services.dart';
 
 import '../../../../common/model/directionModel.dart';
 
@@ -126,45 +128,78 @@ class RideRequestProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  createIcons(BuildContext context) {
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(
+      data.buffer.asUint8List(),
+      targetWidth: width,
+    );
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(
+      format: ui.ImageByteFormat.png,
+    ))!.buffer.asUint8List();
+  }
+
+  createIcons(BuildContext context) async {
     if (pickupIconForMap == null) {
-      ImageConfiguration imageConfiguration = createLocalImageConfiguration(
-        context,
-        size: const Size(32, 32),
-      );
-      BitmapDescriptor.asset(
-        imageConfiguration,
+      // ImageConfiguration imageConfiguration = createLocalImageConfiguration(
+      //   context,
+      //   size: const Size(32, 32),
+      // );
+      // BitmapDescriptor.asset(
+      //   imageConfiguration,
+      //   'assets/images/icons/pickupPng.png',
+      // ).then((icon) {
+      //   pickupIconForMap = icon;
+      //   notifyListeners();
+      // });
+      final Uint8List markerIcon = await getBytesFromAsset(
         'assets/images/icons/pickupPng.png',
-      ).then((icon) {
-        pickupIconForMap = icon;
-        notifyListeners();
-      });
+        30,
+      );
+
+      pickupIconForMap = BitmapDescriptor.bytes(markerIcon);
+      notifyListeners();
     }
     if (destinationIconForMap == null) {
-      ImageConfiguration imageConfiguration = createLocalImageConfiguration(
-        context,
-        size: const Size(48, 48),
-      );
-      BitmapDescriptor.asset(
-        imageConfiguration,
+      // ImageConfiguration imageConfiguration = createLocalImageConfiguration(
+      //   context,
+      //   size: const Size(48, 48),
+      // );
+      // BitmapDescriptor.asset(
+      //   imageConfiguration,
+      //   'assets/images/icons/dropPng.png',
+      // ).then((icon) {
+      //   destinationIconForMap = icon;
+      //   notifyListeners();
+      // });
+      final Uint8List markerIcon = await getBytesFromAsset(
         'assets/images/icons/dropPng.png',
-      ).then((icon) {
-        destinationIconForMap = icon;
-        notifyListeners();
-      });
+        35,
+      );
+
+      destinationIconForMap = BitmapDescriptor.bytes(markerIcon);
+      notifyListeners();
     }
     if (carIconForMap == null) {
-      ImageConfiguration imageConfiguration = createLocalImageConfiguration(
-        context,
-        size: const Size(2, 2),
-      );
-      BitmapDescriptor.asset(
-        imageConfiguration,
+      // ImageConfiguration imageConfiguration = createLocalImageConfiguration(
+      //   context,
+      //   size: const Size(2, 2),
+      // );
+      // BitmapDescriptor.asset(
+      //   imageConfiguration,
+      //   'assets/images/vehicle/mapCar.png',
+      // ).then((icon) {
+      //   carIconForMap = icon;
+      //   notifyListeners();
+      // });
+      final Uint8List markerIcon = await getBytesFromAsset(
         'assets/images/vehicle/mapCar.png',
-      ).then((icon) {
-        carIconForMap = icon;
-        notifyListeners();
-      });
+        35,
+      );
+
+      carIconForMap = BitmapDescriptor.bytes(markerIcon);
+      notifyListeners();
     }
   }
 
